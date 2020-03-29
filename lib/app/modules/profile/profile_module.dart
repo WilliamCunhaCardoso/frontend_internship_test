d3ed0b3110499ea0c87e98069a52e3785233f94e
William Cunha Cardoso <williamcardoso@pm.me>
2020-03-29 09:22:14 -0300

    profile module update

--- lib/app/modules/profile/profile_module.dart
+++ lib/app/modules/profile/profile_module.dart
@@ -1,14 +1,14 @@
-import 'package:frontent_internship_test/app/modules/profile/profile_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
+import 'profile_controller.dart';
+import 'profile_page.dart';
 
 class ProfileModule extends ChildModule {
   @override
-  List<Bind> get binds => [
-        Bind((i) => ProfileController()),
-      ];
+  List<Bind> get binds => [Bind((i) => ProfileController())];
 
   @override
-  List<Router> get routers => [];
+  List<Router> get routers => [Router('/', child: (_, args) => ProfilePage(user: args.data,))];
 
   static Inject get to => Inject<ProfileModule>.of();
 }
--- lib/app/modules/profile/profile_page.dart
+++ lib/app/modules/profile/profile_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:frontent_internship_test/app/modules/profile/widgets/information.dart';
+import 'package:frontent_internship_test/app/modules/user/user_model.dart';
+import 'package:frontent_internship_test/app/shared/circle_avatar.dart';
+import 'package:frontent_internship_test/app/shared/titleSession.dart';
 
 class ProfilePage extends StatefulWidget {
   final String title;
-  const ProfilePage({Key key, this.title = "Profile"}) : super(key: key);
+  final UserModel user;
+
+  const ProfilePage({Key key, this.title = "User Data", this.user})
+      : super(key: key);
 
   @override
   _ProfilePageState createState() => _ProfilePageState();
@@ -14,9 +21,39 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(height: 15),
+            CustomCircleAvatar(
+                text: widget.user.username, fontSize: 50, circleRadius: 56),
+            SizedBox(height: 15),
+            Center(
+                child: Text(widget.user.username,
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.w600))),
+            SizedBox(height: 25),
+            TitleSession(text: 'personal data'),
+            InfoFields(description: 'Email', value: widget.user.email),
+            InfoFields(description: 'Email', value: widget.user.phone),
+            // InfoFields(description: 'Email', value: widget.user.email),
+            InfoFields(description: 'Email', value: widget.user.cpf),
+            TitleSession(text: 'address data'),
+            InfoFields(description: 'Email', value: widget.user.cep),
+            InfoFields(description: 'Email', value: widget.user.street),
+            InfoFields(
+              description: 'Email',
+              value: widget.user.number.toString(),
+            ),
+            InfoFields(description: 'Email', value: widget.user.complement),
+            InfoFields(description: 'Email', value: widget.user.district),
+            InfoFields(description: 'Email', value: widget.user.city),
+            InfoFields(description: 'Email', value: widget.user.state),
+          ],
+        ),
       ),
     );
   }
