a360acbc3d813b7f903ba926296bf7dfc72b5b93
William Cunha Cardoso <williamcardoso@pm.me>
2020-03-29 17:37:26 -0300

    font to simulate prototype font

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -8,13 +8,19 @@ class AppWidget extends StatelessWidget {
       navigatorKey: Modular.navigatorKey,
       title: 'Qwkin Test',
       theme: ThemeData(
+        fontFamily: 'Open Sans',
         primarySwatch: Colors.blue,
         appBarTheme: AppBarTheme(
           elevation: 0,
           iconTheme: IconThemeData(color: Colors.deepPurple, size: 40),
           color: Colors.white,
           textTheme: TextTheme(
-            title: TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 18),
+            title: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+              fontSize: 18,
+              fontFamily: 'Open Sans',
+            ),
           ),
         ),
       ),
